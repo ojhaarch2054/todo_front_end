88932d9290fb34d45e48c6bc3ebe6e35eafc9ff6
Archana Ojha <archana.ozza@gmail.com>
2024-03-16 22:06:58 +0200

    task 5 page 5

--- js/index.js
+++ js/index.js
@@ -11,6 +11,8 @@ input.disabled = true
 const renderTask = (task) => {
     const li = document.createElement('li')
     li.setAttribute('class', 'list-group-item')
+    li.setAttribute('data-key', task.getId().toString())
+    li.innerHTML = task.getText()
     renderSpan(li,task.getText())
     renderLink(li,task.getId())
     list.append(li)
@@ -25,6 +27,16 @@ const renderLink = (li,id) => {
     const a = li.appendChild(document.createElement('a'))
     a.innerHTML = '<i class="bi bi-trash"></i>'
     a.setAttribute('style','float: right')
+    a.addEventListener('click',(event) => {
+        todos.removeTask(id).then((removed_id) => {
+            const li_to_remove = document.querySelector(`[data-key='${removed_id}']`)
+            if(li_to_remove){
+                list.removeChild(li_to_remove)
+            }
+        }).catch((error)=> {
+            alert(error)
+        })
+    })
 }
 
 // To fetch data from server to the frontend
